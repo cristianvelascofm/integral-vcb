7ff68b66f1ca526ca9fdf00927f3ca3ee3e57ab4
Cristian Velasco <cfvelasco@unicauca.edu.co>
2023-04-05 21:47:17 -0500

    menu lateral - login - panelDcoument

--- src/app/document-panel/document-panel.component.ts
+++ src/app/document-panel/document-panel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-document-panel',
   templateUrl: './document-panel.component.html',
-  styleUrls: ['./document-panel.component.css']
+  styleUrls: ['./document-panel.component.scss']
 })
 export class DocumentPanelComponent {
 
--- src/app/principal-menu/principal-menu.component.ts
+++ src/app/principal-menu/principal-menu.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-principal-menu',
   templateUrl: './principal-menu.component.html',
-  styleUrls: ['./principal-menu.component.css']
+  styleUrls: ['./principal-menu.component.scss']
 })
 export class PrincipalMenuComponent {
 
