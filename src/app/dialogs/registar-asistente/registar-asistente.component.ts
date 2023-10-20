5763accccfecd1a98c7b3c09cccf4a76cab77a91
cristianvelascofm <cfvelasco@unicauca.edu.co>
2023-10-20 17:19:28 -0500

    Cargar Asistente Estudiante Activo

--- src/app/dialogs/registar-asistente/registar-asistente.component.ts
+++ src/app/dialogs/registar-asistente/registar-asistente.component.ts
@@ -1,14 +1,11 @@
 import { Component, Inject } from '@angular/core';
 import {
   FormControl,
-  FormGroupDirective,
-  NgForm,
   Validators,
-  FormsModule,
-  ReactiveFormsModule,
 } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Persona, Usuario, Participante, Estudiante, Asistente, PersonaService } from 'src/app/services/persona.service';
 import axios from 'axios';
 
 @Component({
@@ -17,23 +14,43 @@ import axios from 'axios';
   styleUrls: ['./registar-asistente.component.css']
 })
 export class RegistarAsistenteComponent {
-  constructor(public dialogRef: MatDialogRef<RegistarAsistenteComponent>, @Inject(MAT_DIALOG_DATA) public msg: string) {
+  constructor(private personaService: PersonaService, public dialogRef: MatDialogRef<RegistarAsistenteComponent>, @Inject(MAT_DIALOG_DATA) public msg: string) {
   }
   // private path = 'http://192.168.1.100:5050';
   path = 'http://192.168.130.79:5050';
 
-  asistente = {
-    nombre: '',
-    apellido: '',
-    fechaNacimiento: '10/01/2023',
-    genero: 'm', // Establece el valor predeterminado para el género
-    tipoDocumento: 'cedula',
-    numeroDocumento: '',
+  asistente: Asistente = {
+    primerNombre: '',
+    segundoNombre: '',
+    primerApellido: '',
+    segundoApellido: '',
+    ciudad: '',
+    codigo: '',
     direccion: '',
-    celular: '',
-    email: '',
-    institucion: ''
-  };
+    emailInstitucional: '',
+    emailPersonal: '',
+    estado: '',
+    facultad: '',
+    fechaNacimiento: '',
+    genero: '',
+    grado: 0,
+    identificacion: '',
+    tipoDocumento: '',
+    institucion: '',
+    jornada: '',
+    periodo: '',
+    programa: '',
+    regionalizacion: '',
+    sede: '',
+    semestre: 0,
+    sitioWeb: '',
+    telefono: ''
+  }
+
+  nombreCompleto = ''
+  apellidoCompleto = ''
+  documento = ''
+
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   matcher = new ErrorStateMatcher();
   onClickNO(): void {
@@ -41,31 +58,74 @@ export class RegistarAsistenteComponent {
   }
   registrarAsistente() {
 
+
+
+
+  }
+
+  borrarCampos() {
+    this.asistente = {
+      primerNombre: '',
+      segundoNombre: '',
+      primerApellido: '',
+      segundoApellido: '',
+      ciudad: '',
+      codigo: '',
+      direccion: '',
+      emailInstitucional: '',
+      emailPersonal: '',
+      estado: '',
+      facultad: '',
+      fechaNacimiento: '',
+      genero: '',
+      grado: 0,
+      identificacion: '',
+      tipoDocumento: '',
+      institucion: '',
+      jornada: '',
+      periodo: '',
+      programa: '',
+      regionalizacion: '',
+      sede: '',
+      semestre: 0,
+      sitioWeb: '',
+      telefono: ''
+    }
+    // this.documento = '';
+    this.nombreCompleto = '';
+    this.apellidoCompleto = '';
   }
   async onEnter(event: any) {
     if (event.keyCode === 13) { // 13 es el código de tecla para Enter
-      try {
-        const response = await this.cargarEstudianteId('1007178242');
-        const estudiante: any = response.data; // Supongo que el tipo de datos es cualquier cosa, ajústalo según tu estructura de datos
-        console.log('Estudiante', estudiante);
-        return estudiante;
-      } catch (error) {
-        console.error('Error al cargar el evento', error);
-        throw error;
+      
+      if (this.documento !== '') {
+        try {
+          const respuesta = await this.personaService.obtenerEstudianteId(this.documento);
+          const estudiante: any = respuesta.data;
+          this.asistente = estudiante;
+          if (!estudiante['accion']) {
+
+
+            const name = this.asistente['primerNombre']?.toString() + ' ' + this.asistente['segundoNombre']?.toString()
+            const lastName = this.asistente['primerApellido']?.toString() + ' ' + this.asistente['segundoApellido']?.toString()
+            console.log('Estudiante', estudiante);
+            if (name !== undefined && lastName !== undefined) {
+              this.asistente['institucion'] = 'Universidad del Cauca';
+              this.asistente['tipoDocumento'] = 'cedula'
+              this.nombreCompleto = name;
+              this.apellidoCompleto = lastName;
+              return estudiante;
+            }
+          }else{
+            this.borrarCampos();
+          }
+
+
+        } catch (error) {
+          console.error('Error al cargar el Estudiante', error);
+          throw error;
+        }
       }
     }
-  }
-  async cargarEstudianteId(id: string) {
-    const dictSend = {
-      accion: 'cargar-estudiante-id',
-      id: id
-    };
-    try {
-      const response = await axios.post(this.path, dictSend);
-      return response;
-    } catch (error) {
-      throw error;
-    }
-
   }
 }
--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -1,16 +1,86 @@
 import { Injectable } from '@angular/core';
-import { Persona } from '../model/Persona'; // Asegúrate de tener la ruta correcta hacia tu clase Persona
+// import { Persona } from '../model/Persona'; // Asegúrate de tener la ruta correcta hacia tu clase Persona
+import axios from 'axios';
+export interface Persona {
+  identificacion?: string;
+  tipoDocumento?: string;
+  primerApellido?: string;
+  segundoApellido?: string;
+  primerNombre?: string;
+  segundoNombre?: string;
+  genero?: string;
+  fechaNacimiento?: string;
+  emailPersonal?: string;
+  telefono?: string;
+  ciudad?: string;
+  direccion?: string;
+}
+
+export interface Estudiante extends Persona {
+  codigo?: string;
+  emailInstitucional?: string;
+  institucion?: string;
+  facultad?: string;
+  programa?: string,
+  semestre?: number;
+  grado?: number;
+  regionalizacion?: string;
+  sede?: string;
+  jornada?: string;
+  periodo?: string;
+  estado?: string
+}
+
+export interface Usuario extends Persona {
+  institucion: string;
+  dependencia: string;
+  cargo: string;
+  estado: string;
+  usuario: string;
+  password: string;
+  contrato?: string;
+  supervisor?: Usuario;
+}
+
+export interface Participante extends Persona {
+  institucion: string;
+  sitioWeb: string;
+}
+
+export interface Asistente extends Estudiante {
+  sitioWeb?: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
   private personas: Persona[] = [];
+  // private path = 'http://192.168.1.100:5050';
+  path = 'http://192.168.130.79:5050';
+  
+  // BUSCAR ESTUDIANTE POR ID
+  async obtenerEstudianteId(identificacion: string) {
+    const dictSend = {
+      accion: 'cargar-estudiante-id',
+      id: identificacion
+    };
+    try {
+      const response = await axios.post(this.path, dictSend);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
 
   // Método para agregar una nueva persona
   agregarPersona(persona: Persona): void {
     this.personas.push(persona);
   }
+
+
+
 
   // Método para obtener la lista de personas
   obtenerPersonas(): Persona[] {
@@ -18,21 +88,21 @@ export class PersonaService {
   }
 
   // Método para obtener una persona por su ID
-  obtenerPersonaPorId(id: number): Persona | undefined {
-    return this.personas.find(persona => persona.id === id);
+  obtenerPersonaPorId(id: string): Persona | undefined {
+    return this.personas.find(persona => persona.identificacion === id);
   }
 
   // Método para actualizar los detalles de una persona
   actualizarPersona(persona: Persona): void {
-    const index = this.personas.findIndex(p => p.id === persona.id);
+    const index = this.personas.findIndex(p => p.identificacion === persona.identificacion);
     if (index !== -1) {
       this.personas[index] = persona;
     }
   }
 
   // Método para eliminar una persona por su ID
-  eliminarPersona(id: number): void {
-    const index = this.personas.findIndex(persona => persona.id === id);
+  eliminarPersona(id: string): void {
+    const index = this.personas.findIndex(persona => persona.identificacion === id);
     if (index !== -1) {
       this.personas.splice(index, 1);
     }
